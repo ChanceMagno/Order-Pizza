36ffcd614f3ef93cf102878899ccfae816894f10
chance magno <chancemagno@gmail.com>
2017-03-03 10:40:01 -0800

    add toppings var and pizza var, addtoppings form to index.html to test price output on front end.

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,15 @@ function Pizza(size, toppings){
 }
 
 Pizza.prototype.price = function(size, toppings){
-  return this.size * this.toppings.length;
+  return 10 + this.size * this.toppings.length;
 }
 
 $(function(){
   $("form#pizzaBuilder").submit(function(){
     event.preventDefault();
     var size = parseInt($("input:radio[name=size]:checked").val());
-    console.log(size);
+    var toppings = $("input:radio[name=topping]:checked").val();
+    var pizza = new Pizza(size, [toppings]);
+    $("#price").append("<il>" + pizza.price(size, [toppings]) + "</li>");
   });
 });
