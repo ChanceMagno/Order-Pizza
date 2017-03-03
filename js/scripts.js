e3c0c05eba5f77a30df89654706422e95dd04ec4
chance magno <chancemagno@gmail.com>
2017-03-03 09:10:29 -0800

    add prototype 'price' for determining price of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,7 @@ function Pizza(size, toppings){
   this.size = size;
   this.toppings = toppings;
 }
+
+Pizza.prototype.price = function(size, toppings){
+  return this.size * this.toppings.length;
+}
