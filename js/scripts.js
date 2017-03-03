cd49ff6a7e994e907918754616a94084bebae5b9
chance magno <chancemagno@gmail.com>
2017-03-03 11:04:09 -0800

    change price algorithm to be more reasonable and not give first topping for free

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,19 @@ function Pizza(size, toppings){
 }
 
 Pizza.prototype.price = function(size, toppings){
-  return 10 + this.size * this.toppings.length;
+  return 10 + this.size + this.toppings.length;
 }
 
 $(function(){
   $("form#pizzaBuilder").submit(function(){
     event.preventDefault();
+
     var size = parseInt($("input:radio[name=size]:checked").val());
-    var toppings = $("input:radio[name=topping]:checked").val();
+    var toppings = $("input:radio[name=topping1]:checked").val();
+    console.log(toppings);
+
     var pizza = new Pizza(size, [toppings]);
     $("#price").append("<il>" + pizza.price(size, [toppings]) + "</li>");
+
   });
 });
