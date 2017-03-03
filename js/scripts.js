203a34ebedf49f182c4d3dfdb16b44c5f1c4ac00
chance magno <chancemagno@gmail.com>
2017-03-03 10:00:43 -0800

    add submit button to index.html, add variable size to front end of scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,11 @@ function Pizza(size, toppings){
 Pizza.prototype.price = function(size, toppings){
   return this.size * this.toppings.length;
 }
+
+$(function(){
+  $("form#pizzaBuilder").submit(function(){
+    event.preventDefault();
+    var size = parseInt($("input:radio[name=size]:checked").val());
+    console.log(size);
+  });
+});
