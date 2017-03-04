e459eaa1987a9a348097e457cb6daa829929e2ff
chance magno <chancemagno@gmail.com>
2017-03-03 16:46:18 -0800

    end of day commit, got stuck and reverted back

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,23 @@ function Pizza(size, toppings){
 }
 
 Pizza.prototype.price = function(size, toppings){
-  return 10 + this.size + this.toppings.length;
+  return 10 + this.size + this.toppings;
 }
 
 $(function(){
   $("form#pizzaBuilder").submit(function(){
     event.preventDefault();
-
+    debugger;
     var size = parseInt($("input:radio[name=size]:checked").val());
-    var toppings = $("input:radio[name=topping1]:checked").val();
-    console.log(toppings);
-
-    var pizza = new Pizza(size, [toppings]);
-    $("#price").append("<il>" + pizza.price(size, [toppings]) + "</li>");
-
+    var toppings =
+    parseInt($("input:checkbox[name=topping1]:checked").val()) + parseInt($("input:checkbox[name=topping2]:checked").val()) + parseInt($("input:checkbox[name=topping3]:checked").val()) + parseInt($("input:checkbox[name=topping4]:checked").val()) +
+    parseInt($("input:checkbox[name=topping5]:checked").val()) +
+    parseInt($("input:checkbox[name=topping6]:checked").val()) +
+    parseInt($("input:checkbox[name=topping7]:checked").val()) +
+    parseInt($("input:checkbox[name=topping8]:checked").val()) +
+    parseInt($("input:checkbox[name=topping9]:checked").val()) +
+    parseInt($("input:checkbox[name=topping10]:checked").val());
+    var pizza = new Pizza(size, toppings);
+    $("#priceOutput").append("<p>" + pizza.price(size, toppings) + "</p>");
   });
 });
