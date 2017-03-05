a4ef69cb44ae5aabfbf207a27934a289107ff9a2
chance <chancemagno@gmail.com>
2017-03-04 17:17:33 -0800

    fixed js prototype'pizzaPrice' to use Pizza object, add function to get value of topping checked boxes only. **this was done after 5pm deadline, I got stuck with a problem and went down a rabbit hole

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,28 @@
-
+//Back End
 function Pizza(size, toppings){
   this.size = size;
   this.toppings = toppings;
 }
 
-Pizza.prototype.price = function(size, toppings){
-  return 10 + this.size + this.toppings;
+
+Pizza.prototype.pizzaPrice = function() {
+     return this.toppings.length + this.size;
 }
 
+
+//front End
 $(function(){
   $("form#pizzaBuilder").submit(function(){
     event.preventDefault();
-    debugger;
-    var size = parseInt($("input:radio[name=size]:checked").val());
-    var toppings =
-    parseInt($("input:checkbox[name=topping1]:checked").val()) + parseInt($("input:checkbox[name=topping2]:checked").val()) + parseInt($("input:checkbox[name=topping3]:checked").val()) + parseInt($("input:checkbox[name=topping4]:checked").val()) +
-    parseInt($("input:checkbox[name=topping5]:checked").val()) +
-    parseInt($("input:checkbox[name=topping6]:checked").val()) +
-    parseInt($("input:checkbox[name=topping7]:checked").val()) +
-    parseInt($("input:checkbox[name=topping8]:checked").val()) +
-    parseInt($("input:checkbox[name=topping9]:checked").val()) +
-    parseInt($("input:checkbox[name=topping10]:checked").val());
-    var pizza = new Pizza(size, toppings);
-    $("#priceOutput").append("<p>" + pizza.price(size, toppings) + "</p>");
+    var sizePrice = parseInt($("input:radio[name=size]:checked").val());
+    var selectedToppings= [];
+
+    $('input[type=checkbox]').each(function(){
+      if (this.checked) {
+        selectedToppings.push($(this).val());
+      }
+    })
+    newOrder = new Pizza(sizePrice, selectedToppings);
+      $("#priceOutput").append("<p>" + newOrder.pizzaPrice() + "</p>");
   });
 });
