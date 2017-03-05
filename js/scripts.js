3097eac45f225b22391f160033c5525f69eb3cd1
chance <chancemagno@gmail.com>
2017-03-05 12:53:45 -0800

    add empty method for priceOutput and toppingList will clear every new submit, adjusted topping price to 1.50 per topping, topping list will now output selected topping(s)

--- js/scripts.js
+++ js/scripts.js
@@ -6,23 +6,28 @@ function Pizza(size, toppings){
 
 
 Pizza.prototype.pizzaPrice = function() {
-     return this.toppings.length + this.size;
+     return this.toppings.length *1.5 + this.size;
 }
 
 
 //front End
 $(function(){
   $("form#pizzaBuilder").submit(function(){
+    $("#priceOutput").empty();
+    $("#toppingsList").empty();
     event.preventDefault();
     var sizePrice = parseInt($("input:radio[name=size]:checked").val());
     var selectedToppings= [];
-
     $('input[type=checkbox]').each(function(){
       if (this.checked) {
         selectedToppings.push($(this).val());
       }
     })
     newOrder = new Pizza(sizePrice, selectedToppings);
-      $("#priceOutput").append("<p>" + newOrder.pizzaPrice() + "</p>");
+      $("#priceOutput").append("<p> Your Total is $" + newOrder.pizzaPrice() + "</p>");
+      selectedToppings.forEach(function(selectedtopping){
+        $("#toppingsList").append("<li>" + selectedtopping + "</li>");
+
+      })
   });
 });
